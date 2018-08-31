7206af9303327aedfe9779fd9cdf4fe45f2e967e
the-sides <jacob798@gmail.com>
2018-08-30 21:03:57 -0400

    fixed #1 with template CSRF

--- djsite/settings.py
+++ djsite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CSRF_USE_SESSIONS = True
 
 # Application definition
 
--- djsite/views.py
+++ djsite/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+from ranker.models import *
 
 def homepage(request):
     return render(request,'homepage.html')
@@ -6,15 +11,19 @@ def homepage(request):
 
 def ajax_new_session(request):
     response_data = {'result':False}
+    print("started", request.method)
     if request.method == 'POST':
 
         try:
             sesh = session()
+            print("sesh made")
             ajax_data = json.loads(request.POST.get('data')) # json.loads allow data to be a stringified json
+            # ajax_data = request.POST.get('data')
+            print("ajax data", ajax_data['sid'])
             sesh.sid = ajax_data['sid']
             sesh.name = ajax_data['pname']
-            sesh.token = ajax_data['token']
-            sesh.playlist_uri = ajax_data['PURI']
+            # sesh.token = ajax_data['token']
+            # sesh.playlist_uri = ajax_data['PURI']
 
             sesh.save()
             response_data['result'] = True
@@ -22,7 +31,7 @@ def ajax_new_session(request):
             # Validator
             status = session.objects.get(id=0)
             print(status.name)
+            print(response_data)
 
-        except Exception as error: response_data['error'] = error
-            
-    return HttpResponse(json.dumps(response_data), content_type='application/json')
+        except Exception as error: print(error)
+    return JsonResponse(response_data, safe=False)
