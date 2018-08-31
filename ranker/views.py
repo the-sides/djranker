24fcd0bfc83e9c0e6180e019355c70478f3573ee
the-sides <jacob798@gmail.com>
2018-08-31 03:38:53 -0400

    mutual view imports

--- ranker/views.py
+++ ranker/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+from django.shortcuts import render
+
 
 # Create your views here.
 
