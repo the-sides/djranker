cae9048e69d5c15bee06995ab272c386ddbfd53e
the-sides <jacob798@gmail.com>
2018-08-31 13:46:22 -0400

    more operations around option states

--- static/ranker/js/base.js
+++ static/ranker/js/base.js
@@ -3,6 +3,6 @@
             Fucking tools bro! It's about time I make a protocol on top of the mechanics
             
                 HERE ARE THE JQEURY EVENT LISTENERS I CAN CONTINUOUSLY USE...
+                forgot what I wanted to write so I'll have to come back
 */
 
-function()
