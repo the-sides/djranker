6f7409baab9d242936863060740192b7cd01978e
the-sides <jacob798@gmail.com>
2018-08-31 13:20:24 -0400

    using global jquery listeners

---
+++ static/ranker/js/base.js
@@ -0,0 +1,8 @@
+/* 
+
+            Fucking tools bro! It's about time I make a protocol on top of the mechanics
+            
+                HERE ARE THE JQEURY EVENT LISTENERS I CAN CONTINUOUSLY USE...
+*/
+
+function()
