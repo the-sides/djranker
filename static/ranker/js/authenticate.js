d440b2272e3cce8d2a1bbc4367dc06a074c57b21
the-sides <sides798@gmail.com>
2018-10-09 10:59:39 -0400

    authentication done through specific view with postMessage function for passing access codes, homepage needs this implementation

---
+++ static/ranker/js/authenticate.js
@@ -0,0 +1,12 @@
+function getToken(){
+    var codeInd = window.location.href.indexOf("=")
+    var codeEndInd = window.location.href.indexOf("&",codeInd)
+    var token = window.location.href.substring(codeInd+1,codeEndInd)
+    // console.log(token);
+    if (token.length !== 210) { 
+        return -1;
+     }
+    return token;
+}
+
+window.opener.postMessage(getToken(),"*")
